8331568a87717fceb84f85b1d0a7879ac5b5ddec
minsu0707 <mandoo4137@gmail.com>
2024-10-26 20:53:09 +0900

    add :: img, font

--- public/App.jsx
+++ public/App.jsx
@@ -5,7 +5,11 @@ import { Diary } from "./pages/Diary";
 import { New } from "./pages/New";
 import { Notfound } from "./pages/NotFound";
 
-import { getEmotionImage } from "./util/get-emotion-image";
+import emotion1 from "./assets/emotion1.png";
+import emotion2 from "./assets/emotion2.png";
+import emotion3 from "./assets/emotion3.png";
+import emotion4 from "./assets/emotion4.png";
+import emotion5 from "./assets/emotion5.png";
 
 function App() {
   const nav = useNavigate();
@@ -17,11 +21,11 @@ function App() {
   return (
     <>
       <div>
-        <img src={getEmotionImage(1)} alt="emotion1" />
-        <img src={getEmotionImage(2)} alt="emotion2" />
-        <img src={getEmotionImage(3)} alt="emotion3" />
-        <img src={getEmotionImage(4)} alt="emotion4" />
-        <img src={getEmotionImage(5)} alt="emotion5" />
+        <img src={emotion1} alt="emotion1" />
+        <img src={emotion2} alt="emotion2" />
+        <img src={emotion3} alt="emotion3" />
+        <img src={emotion4} alt="emotion4" />
+        <img src={emotion5} alt="emotion5" />
       </div>
       <div>
         <Link to={"/"}>Home</Link>
