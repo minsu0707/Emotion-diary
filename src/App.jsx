a870eb88c9d974f04f6f56f0eb35c2ebd79e73b5
minsu0707 <mandoo4137@gmail.com>
2024-10-26 19:28:08 +0900

    add :: 페이지 이동

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Diary } from "./pages/Diary";
 import { New } from "./pages/New";
 import { Notfound } from "./pages/NotFound";
 
 function App() {
+  const nav = useNavigate();
+
+  const onClickButton = () => {
+    nav("/new");
+  };
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="new" element={<New />} />
-      <Route path="/diary" element={<Diary />} />
-      <Route path="*" element={<Notfound />} />
-    </Routes>
+    <>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
+      </div>
+      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="new" element={<New />} />
+        <Route path="/diary" element={<Diary />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </>
   );
 }
 
 export default App;
- 
