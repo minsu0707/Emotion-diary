019e7e7f4bb8ec6a176cba238ad04b37dc45c213
minsu0707 <mandoo4137@gmail.com>
2024-10-28 08:54:42 +0900

    add :: 공통 컴포넌트

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,32 @@
 import "./App.css";
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Diary } from "./pages/Diary";
 import { New } from "./pages/New";
 import { Notfound } from "./pages/NotFound";
-
-import { getEmotionImage } from "./util/get-emotion-image";
+import { Button } from "./components/Button";
+import { Header } from "./components/Header";
 
 function App() {
-  const nav = useNavigate();
-
-  const onClickButton = () => {
-    nav("/new");
-  };
-
   return (
     <>
-      <div>
-        <img src={getEmotionImage(1)} alt="emotion1" />
-        <img src={getEmotionImage(2)} alt="emotion2" />
-        <img src={getEmotionImage(3)} alt="emotion3" />
-        <img src={getEmotionImage(4)} alt="emotion4" />
-        <img src={getEmotionImage(5)} alt="emotion5" />
-      </div>
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/new"}>New</Link>
-        <Link to={"/diary"}>Diary</Link>
-      </div>
-      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Header
+        title={"Header"}
+        leftChild={<Button text={"Left"} />}
+        rightChild={<Button text={"Right"} />}
+      />
+
+      <Button text={"감정일기장"} onClick={() => console.log("123!")} />
+      <Button
+        text={"감정일기장"}
+        type={"POSITIVE"}
+        onClick={() => console.log("123!")}
+      />
+      <Button
+        text={"감정일기장"}
+        type={"NEGATIVE"}
+        onClick={() => console.log("123!")}
+      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="new" element={<New />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,7 @@
+import { useSearchParams } from "react-router-dom";
+
 export const Home = () => {
+  const [params, setParams] = useSearchParams();
+
   return <div>Home</div>;
 };
